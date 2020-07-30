37e29746a048c657d6a5fbd21c67c49647450960
cullyRSG <63077969+cullyRSG@users.noreply.github.com>
2020-07-29 21:57:12 -0500

    Update phonebanking_install.sql
    No changes to code, just improved formatting.

--- sql/phonebanking_install.sql
+++ sql/phonebanking_install.sql
@@ -1,4 +1,36 @@
-SELECT @activityType := cog.id, @value := max(cast(value as unsigned)) + 1 FROM civicrm_option_group cog INNER JOIN civicrm_option_value cov ON cov.option_group_id = cog.id WHERE  cog.name = 'activity_type';
+SELECT @activitytype := cog.id, 
+       @value := Max(Cast(value AS UNSIGNED)) + 1 
+FROM   civicrm_option_group cog 
+       INNER JOIN civicrm_option_value cov 
+               ON cov.option_group_id = cog.id 
+WHERE  cog.name = 'activity_type'; 
 
-INSERT INTO civicrm_option_value(option_group_id, label, value, name, grouping, filter, is_default, weight, description, is_optgroup, is_reserved, is_active, component_id, visibility_id) 
-VALUES (@activityType, 'CallHub Phonebank Activity', @value, 'CallHub Phonebank Activity', NULL, 0, 0, @value, 'CallHub Phonebank Activity', 0, 1, 1, NULL, NULL);
+INSERT INTO civicrm_option_value 
+            (option_group_id, 
+             label, 
+             value, 
+             name, 
+             grouping, 
+             filter, 
+             is_default, 
+             weight, 
+             description, 
+             is_optgroup, 
+             is_reserved, 
+             is_active, 
+             component_id, 
+             visibility_id) 
+VALUES      (@activitytype, 
+             'CallHub Phonebank Activity', 
+             @value, 
+             'CallHub Phonebank Activity', 
+             NULL, 
+             0, 
+             0, 
+             @value, 
+             'CallHub Phonebank Activity', 
+             0, 
+             1, 
+             1, 
+             NULL, 
+             NULL); 
